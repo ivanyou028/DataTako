b8259402ec5aa1e57ece86d9bf8d3c77db3b8b89
Ivan You <youy@fb.com>
2023-08-16 03:11:50 -0700

    add openai key input box

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Import necessary libraries and modules
 import streamlit as st
-import pandas as pd
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 import openai
@@ -10,18 +9,12 @@ from util import load_df
 # Get API key
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
-# Set OpenAI API key
-openai.api_key = OPENAI_API_KEY
-
 # Set page configuration and title for Streamlit
 st.set_page_config(page_title="DataTako", page_icon="🐙", layout="wide")
 
 data_files = []
 
-llm = OpenAI(api_token=OPENAI_API_KEY)
-pandas_ai = PandasAI(llm, middlewares=[StreamlitMiddleware()], custom_whitelisted_dependencies=["scikit-learn"])
-
-def chat_with_data(dfs, prompt):
+def chat_with_data(dfs, prompt, pandas_ai):
     result = pandas_ai.run(dfs, prompt=prompt, is_conversational_answer=False, show_code=True)
     print(result)
     return result
@@ -31,15 +24,18 @@ with st.sidebar:
     st.markdown(
         '<p style="display:inline-block;font-size:40px;font-weight:bold;">🐙DataTako </p>'
         ' <p style="display:inline-block;font-size:16px;">🐙DataTako is a tool that uses AI-powered '
-        'natural language processing to analyze and provide insights on structured data. Users can '
+        'natural language processing to analyze and provide insights on structured data. You can '
         'upload files, view the data, and have interactive conversations with the AI model '
-        'to obtain valuable information and answers related to the uploaded data <br><br></p>',
+        'to understand the data. </p>',
         unsafe_allow_html=True
     )
+    st.divider()
+    if not OPENAI_API_KEY:
+        OPENAI_API_KEY = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password", placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     data_files = st.file_uploader("Upload your data file", type=['csv', 'xlsx', 'json', 'txt'], accept_multiple_files=True)
 
 if not len(data_files):
-    st.spinner("Waiting for input file")
+    pass
 else:
     st.toast("File Uploaded Successfully", icon='🎉')
     dataframes = [load_df(file) for file in data_files]
@@ -53,7 +49,10 @@ else:
         if not input_text:
             st.error('Please provide a question', icon="🚨")
         else:
-            result = chat_with_data(dataframes, input_text)
+            openai.api_key = OPENAI_API_KEY
+            llm = OpenAI(api_token=OPENAI_API_KEY)
+            pandas_ai = PandasAI(llm, middlewares=[StreamlitMiddleware()], custom_whitelisted_dependencies=["scikit-learn"])
+            result = chat_with_data(dataframes, input_text, pandas_ai)
             st.success(result)
             expander = st.expander("See Code")
             expander.code(pandas_ai.last_code_executed)
@@ -63,9 +62,7 @@ else:
 # Hide Streamlit header, footer, and menu
 hide_st_style = """
     <style>
-    #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    header {visibility: hidden;}
     </style>
 """
 
